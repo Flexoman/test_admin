33287017ecbcef07274655275dc2d062e25cedb9
Andriy <andriuhorak@gmail.com>
2020-01-20 04:01:31 +0200

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, "active_admin"
 set :repo_url, 'git@github.com:Flexoman/test_admin.git'
 
 set :linked_files, %w{.env}
-append :linked_dirs, 'tmp/pids', 'tmp/cache', 'tmp/sockets'
+append :linked_dirs, 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'logs'
 
 set :deploy_user, 'deploy'
 set :deploy_to, "/home/#{fetch(:deploy_user)}/hiro/#{fetch(:application)}"
@@ -31,3 +31,5 @@ set :rake_roles, [:web, :db, :app]
 
 set :puma_user, fetch(:deploy_user)
 set :puma_conf, "#{shared_path}/puma.rb"
+set :puma_access_log, File.join(shared_path, 'logs', 'puma_access.log')
+set :puma_error_log, File.join(shared_path, 'logs', 'puma_error.log')
