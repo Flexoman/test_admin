4b35e2ca6998e7a7e91b8a14ef8dd9a6921e264c
Andriy <andriuhorak@gmail.com>
2020-03-30 16:07:33 +0300

    move to subdomain

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :db_admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
+
+  constraints host: ENV['ADMIN_HOST'] do
+    devise_for :db_admin_users, ActiveAdmin::Devise.config.merge(:path => '')
+    ActiveAdmin.routes(self)
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
