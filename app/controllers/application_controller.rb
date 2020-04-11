e901bed78b1ec9e1aca521f0bd57adcca991e278
Andriy <andriuhorak@gmail.com>
2020-04-11 18:53:57 +0300

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :show_session
+
+  def show_session
+    ap session.keys
+    ap session.values
+  end
+
 end
